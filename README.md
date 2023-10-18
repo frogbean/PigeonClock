6f20866cb53e69da3184fa2ad7940abdab1d8897
frogbean <49106938+frogbean@users.noreply.github.com>
2023-10-18 16:33:39 +0100

    Mostly done

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # PigeonClock
+
+Create a json with your coordinates
+
+Example 
+
+```json
+{
+    "latitude" : 50,
+    "longitude" : 5
+}
+```
+
+Save it as `coords.json`
+
+Now you can use `alarms.json` like so
+
+```json
+{
+    "Pigeon breakfast" : {
+        "message" : "Time to feed the pigeons their breakfast!",
+        "when" : 0.25
+    },
+    "Pigeon dinner" : {
+        "message" : "Time to feed the pigeons their dinner!",
+        "when" : 0.75
+    }
+}
+```
--- main.js
+++ main.js
@@ -1,3 +1,60 @@
 const suncalc = require("suncalc");
 const alarms = require("./alarms.json");
-console.log("Hello Wrodsl");
+
+try {
+    require("./coords.json");
+} catch {
+console.warn("You need to create a coords.json file containing your latitude and longitude values")
+console.warn(`Example 
+
+{
+    "latitude" : 50,
+    "longitude" : 5
+}
+
+Save it as coords.json`)
+    process.exit(0);
+}
+const { latitude, longitude } = require("./coords.json");
+
+function sunAngle(when = new Date()) {
+    return (suncalc.getPosition(when, latitude, longitude).altitude * 100);
+}
+
+function sunAngleMax() {
+    const noon = suncalc.getTimes(new Date(), latitude, longitude).solarNoon;
+    return sunAngle(noon);
+}
+
+function sunAngleMin() {
+    const unnoon = suncalc.getTimes(new Date(), latitude, longitude).nadir;
+    return sunAngle(unnoon);
+}
+
+function pClock() {
+    pClock.sunMax ??= sunAngleMax();
+    pClock.sunMin ??= sunAngleMin();
+    const sunAng = sunAngle();
+
+    if(sunAng >= 0) {
+        const noon = suncalc.getTimes(new Date(), latitude, longitude).solarNoon;
+        const phase = sunAng / pClock.sunMax;
+        if(new Date() <= noon)
+            return phase / 2;
+        else 
+            return 0.5 + ((1 - phase) / 2);
+    } else {
+        const unnoon = suncalc.getTimes(new Date(), latitude, longitude).nadir;
+        const phase = sunAng / pClock.sunMin;
+        if(new Date() <= unnoon)
+            return phase / 2;
+        else 
+            return -0.5 - ((-1 + phase) / 2);
+    }
+}
+
+setInterval(()=>{
+    console.clear();
+    console.log(sunAngle().toFixed(2), '⊾');
+    console.log((pClock() * 100).toFixed(2), '%');
+}, 100);
